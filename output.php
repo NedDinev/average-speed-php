325dfeb32a58824122cef32c17bb200afdc4aa0b
Nedelcho Dinev <ned.dinev@gmail.com>
2022-04-13 17:40:28 +0300

    Add files via upload

--- output.php
+++ output.php
@@ -7,18 +7,33 @@
     <title>Средна скорост</title>
   </head>
   <body>
-      <h2>Средна скорост</h2>
+      <h2>Изчисляване на средна скорост</h2>
 <?php 
 $put=$_POST["put"];
 $trugvaneChas=$_POST["trugvaneChas"];
 $trugvaneMin=$_POST["trugvaneMin"];
 $pristiganeChas=$_POST["pristiganeChas"];
 $pristiganeMin=$_POST["pristiganeMin"];
+$chas = strval(($pristiganeChas - $trugvaneChas));
+$minuti = strval(($pristiganeMin - $trugvaneMin));
+$srednaSkorost =  strval($chas . $minuti);
+$solve = round(($put / (intval($srednaSkorost) )), 2);
+if ($solve < 1){
+  $solve = round(($put / (intval($srednaSkorost)/100 )), 2);
+}
+else if($solve >=1 ){
+  $solve = round(($put / (intval($srednaSkorost)/10 )), 2);
+}
 
 ?>
 <table border="4" bordercolor="black" cellspacing="0" cellpadding="9" width="50%"> 
     <caption><big><b>Време на пристигане</b></big></caption>
-<tr><th>Път: </th><?php echo $trugvaneChas; ?> </tr> 
+<tr><th>Път за изминаване:</th><th><?php echo $put; ?> </th></tr> 
+<tr><th>Време на тръгване в часове:</th><th><?php echo $trugvaneChas; ?> </th></tr> 
+<tr><th>Време на тръгване в минути:</th><th><?php echo $trugvaneMin; ?> </th></tr> 
+<tr><th>Време на пристигане в часове: </th><th><?php echo $pristiganeChas; ?> </th></tr> 
+<tr><th>Време на пристигане в минути:</th><th><?php echo $pristiganeMin; ?> </th></tr> 
+<tr><th>Средна скорост км/ч:</th><th><?php  echo $solve  ?> </th></tr> 
 </table>
 </body>
 </html>
